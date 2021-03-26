b6510250a26a3bc9a58d3f13962d0efdb3d8b8be
= <=>
2021-03-26 12:33:45 +0100

    make Giovanni VIII page

--- app.js
+++ app.js
@@ -10,19 +10,31 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
+// Front page
 app.get("/", function (req, res) {
 
     res.render("carousel");
 });
+
+// About Section
 app.get("/about", function (req, res) {
     res.render("about");
 });
+
+// Gibsy Section
 app.get("/gibsy", function (req, res) {
     res.render("gibsy");
 });
+
+// Pane di Ferro Section
 app.get("/panediferro", function (req, res) {
     res.render("panediferro");
+});
+
+
+// Giovanni VIII Section
+app.get("/giovanni", function (req, res) {
+    res.render("giovanni");
 });
 
 
@@ -31,6 +43,6 @@ app.get("/panediferro", function (req, res) {
 // });
 
 
-app.listen(process.env.PORT || 300, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server started on port 3000.");
 });
