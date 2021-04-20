8004b52b77aaaf4697cf8f896bff57c42b55b735
= <=>
2021-04-20 14:51:07 +0200

    add hover effect on gallery

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $('.gallery-item').click(function () {
         $('#exampleModal').modal('show')
-        $('.modal-img').attr("src", $(this).attr("src"))
+        $('.modal-img').attr("src", $(this).find("img:first").attr("src"))
     });
 });
